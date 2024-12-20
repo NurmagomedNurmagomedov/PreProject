41b893928b39a80a8e6a51b89b87a2ec533c7bca
NurmagomedNurmagomedov <nnurmagomedov@gmail.com>
2024-12-20 15:54:09 +0300

    PreProject 2.1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,8 +5,6 @@ import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-import org.hibernate.Session;
 
 public class Main {
     public static void main(String[] args) {
